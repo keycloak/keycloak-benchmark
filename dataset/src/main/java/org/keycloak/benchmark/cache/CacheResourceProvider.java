eee987c8f470cfdaf490f8d5598b6ce1662e01f7
Joerg Matysiak <joerg.matysiak@bosch.io>
2021-11-04 16:57:45 +0100

    * Make dataset spi work with keycloak 16

--- dataset/src/main/java/org/keycloak/benchmark/cache/CacheResourceProvider.java
+++ dataset/src/main/java/org/keycloak/benchmark/cache/CacheResourceProvider.java
@@ -33,8 +33,8 @@ import org.infinispan.manager.EmbeddedCacheManager;
 import org.jboss.resteasy.annotations.cache.NoCache;
 import org.keycloak.benchmark.dataset.TaskResponse;
 import org.keycloak.connections.infinispan.InfinispanConnectionProvider;
+import org.keycloak.connections.infinispan.InfinispanUtil;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.sessions.infinispan.util.InfinispanUtil;
 import org.keycloak.services.resource.RealmResourceProvider;
 import org.keycloak.utils.MediaType;
 
