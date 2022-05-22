23007f1366ae6f8b72ce9f98cd120e841f863601
Alexander Schwartz <alexander.schwartz@gmx.net>
2022-05-22 18:04:41 +0200

    adding loki to the mix

--- provision/minikube/allinone.sh
+++ provision/minikube/allinone.sh
@@ -17,6 +17,8 @@ kubectl create namespace monitoring
 helm install prometheus prometheus-community/kube-prometheus-stack -f monitoring.yaml
 helm install monitoring --set hostname=$(minikube ip).nip.io monitoring
 helm install tempo grafana/tempo -n monitoring -f tempo.yaml
+helm install loki grafana/loki -n monitoring -f loki.yaml
+helm install promtail grafana/promtail -n monitoring -f promtail.yaml
 helm install keycloak --set hostname=$(minikube ip).nip.io keycloak
 echo -e "use\n\n  kubectl get pods -A -w\n\nto get information about starting pods\n\n"
 echo -e "use\n\n  ./isup.sh\n\nto wait for all services to become available\n\n"
