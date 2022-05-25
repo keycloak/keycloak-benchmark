1b300ab3fe83a82e2d1d6a3ec6121230f4716ba1
kameshakella <kakella@redhat.com>
2022-05-25 16:54:12 +0530

    Add a bash utility to automate the service account enabled client management steps (fixes #75)
    A simple bash utility which consumes the kcadm.sh CLI script bundled with the Keycloak distributions. Detailed instructions are update in the README for the benchmark module

---
+++ benchmark/manage_gatling_client.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+#Functions
+function usage {
+  echo "Usage: $(basename $0) -k KEYCLOAK_HOME -r REALM_NAME -c CLIENT_ID -s SERVER_URL [-d]" 2>&1
+  echo '-k keycloak home path ex: /home/opt/keycloak-18.0.0 '
+  echo '-r realm : default value realm-0'
+  echo '-c service account enabled client name : default value gatling'
+  echo '-s keycloak host server url : default value http://localhost:8081/auth'
+  echo '-d delete the client and realm : default value is false'
+  exit 1
+}
+
+function install_kcb {
+  echo "Installing kcadm.sh"
+  export PATH=$PATH:$KEYCLOAK_HOME/bin
+}
+
+function create_realm {
+  if kcadm.sh get realms  | grep \"realm\" | grep -q $REALM_NAME; then
+    echo "INFO: Skipping Realm creation as realm exists"
+  else
+    echo "INFO: Creating Realm with realm id : ${REALM_NAME}"
+    kcadm.sh create realms -s realm=$REALM_NAME -s enabled=true -o
+  fi
+}
+
+function create_client_assign_roles {
+  #Create the client application with Service Account Roles setup
+  CID=$(kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_ID -s enabled=true -i)
+  echo "INFO: Created New ${CLIENT_ID} Client with Client ID ${CID}"
+  #Update the client with necessary attributes
+  kcadm.sh update clients/$CID -r $REALM_NAME  -s "secret=4CYUuDEYvvAkjIcjcB0EgAJxmbXdgMYR" -s serviceAccountsEnabled=true  -s publicClient=false -s 'redirectUris=["http://localhost:8081"]'
+  kcadm.sh get clients/$CID -r $REALM_NAME --fields clientId,id,redirectUris,publicClient,serviceAccountsEnabled,secret
+
+  #Verify if the service account user exists
+  kcadm.sh get users -r $REALM_NAME -q username=service-account-$CLIENT_ID --fields id,username,enabled
+
+  #Assign the necessary Service Account based roles to the client
+  kcadm.sh add-roles -r $REALM_NAME --uusername service-account-$CLIENT_ID --cclientid realm-management --rolename manage-clients --rolename view-users --rolename manage-realm --rolename manage-users
+  #kcadm.sh get-roles -r $REALM_NAME --uusername service-account-$CLIENT_ID --cclientid realm-management
+}
+
+function delete_entities {
+  CID=$(kcadm.sh get clients -r realm-0 --fields id,clientId | grep gatling -B 1 | grep id | cut -d ":" -f 2 | sed -e 's/,//g' | sed -e 's/"//g' | sed -e 's/ //g')
+  echo "$CID"
+  kcadm.sh delete clients/$CID -r $REALM_NAME
+  kcadm.sh delete realms/$REALM_NAME
+}
+
+#main()
+#setting default values
+OPTIND=1
+REALM_NAME=realm-0
+CLIENT_ID=gatling
+SERVER_URL="http://localhost:8081/auth"
+DELETE_ENTITIES='false'
+
+while getopts ":skrcd:" arg; do
+  case $arg in
+    s) SERVER_URL="$OPTARG";
+        ;;
+    k) KEYCLOAK_HOME="$OPTARG";
+        ;;
+    r) REALM_NAME="$OPTARG";
+        ;;
+    c) CLIENT_ID="$OPTARG";
+        ;;
+    d) DELETE_ENTITIES="$OPTARG"; DELETE_ENTITIES='true'; 
+        ;;
+    \?) echo "ERROR: Invalid option: -${OPTARG}" >&2
+        usage
+        ;;
+  esac
+done
+
+#Handle missing opt args
+if (( ${OPTIND} == 1 ))
+then
+  echo "ERROR: No Options Specified"
+  usage
+fi
+shift $(( OPTIND -1 ))
+
+#Setup the KCB client tool in the system PATH
+install_kcb
+
+#Create or Re-Create Client and Realm
+if ${DELETE_ENTITIES}; then
+ echo "INFO: deleting the Client and Realm"
+ delete_entities
+else
+ echo "WARN: not deleting the Client and Realm"
+fi
+
+create_realm
+create_client_assign_roles
