fcb0734bfc38b6f24ced9d7eece610ef49435995
Alexander Schwartz <aschwart@redhat.com>
2022-07-21 21:37:08 +0200

    Adding Jaeger retention in hours, not days
    Closes #144

--- provision/minikube/isup.sh
+++ provision/minikube/isup.sh
@@ -17,7 +17,7 @@ declare -A SERVICES=( \
  ["grafana.${HOST}"]="" \
  ["prometheus.${HOST}"]="" \
  ["sqlpad.${HOST}"]="" \
- ["jaeger.${HOST}"]="" \
+ ["jaeger.${HOST}"]="api/services" \
  ["kubebox.${HOST}"]="" \
  ["cryostat.${HOST}"]="" \
  )
@@ -27,11 +27,6 @@ for SERVICE in "${!SERVICES[@]}"; do
   # loop until we connect successfully or failed
   until kubectl get ingress -A 2>/dev/null | grep ${SERVICE} >/dev/null && curl -k -f -v https://${SERVICE}/${SERVICES[${SERVICE}]} >/dev/null 2>/dev/null
   do
-    if [ "${RETRIES}" == "${MAXRETRIES}" ] && [ "${CI}" != "true" ]
-    then
-      echo -n "Waiting for services to start on ${SERVICE}"
-    fi
-
     RETRIES=$(($RETRIES - 1))
     if [ $RETRIES -eq 0 ]
     then
@@ -44,5 +39,24 @@ for SERVICE in "${!SERVICES[@]}"; do
     fi
     sleep 5
   done
+
+  if [ "${SERVICE}" == "jaeger.${HOST}" ]
+  then
+    until curl -k -f -v https://${SERVICE}/${SERVICES[${SERVICE}]} -o - 2>/dev/null | grep "jaeger-query" >/dev/null 2>/dev/null
+    do
+      RETRIES=$(($RETRIES - 1))
+      if [ $RETRIES -eq 0 ]
+      then
+          echo "Failed to see service jaeger-query in the list of Jaeger services"
+          exit 1
+      fi
+      # wait a bit
+      if [ "$GITHUB_ACTIONS" == "" ]; then
+        echo -n "."
+      fi
+      sleep 5
+    done
+  fi
+
   echo https://${SERVICE}/ is up
 done
