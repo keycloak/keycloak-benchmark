0a0c6eec79c5e8dc0c6e3916bb2c6ea1ccce14f3
kameshakella <kakella@redhat.com>
2022-09-15 22:40:38 -0400

    Adding new capabilities to initialize-benchmark-entities.sh (fixes #159)
    Renaming the manage-gatling-client.sh to initialize-benchmark-entities.sh
    
    Adding new user creation capabilities to the script to create a default user to be able to kick off a single user Authentication scenarios (LoginUserPassword, ClientSecret, AuthorizationCode)
    
    Updating the docs to reflect the latest state

--- benchmark/initialize-benchmark-entities.sh
+++ benchmark/initialize-benchmark-entities.sh
@@ -7,11 +7,12 @@ set -o pipefail
 function usage {
   echo "Setup of test clients for the benchmark test with a pre-defined secret"
   echo "Usage: $(basename $0) -k KEYCLOAK_HOME -r REALM_NAME -c CLIENT_ID [-d]" 2>&1
-  echo '-k keycloak home path ex: /home/opt/keycloak-18.0.0 : default value KEYCLOAK_HOME env variable'
-  echo '-d delete the client and realm before re-creating them: default value is false'
-  echo '-r realm : default value realm-0'
-  echo '-c service account enabled client name : default value gatling'
-  echo '-d delete the client and realm : default value is false'
+  echo "-k keycloak home path ex: /home/opt/keycloak-18.0.0 : default value KEYCLOAK_HOME env variable"
+  echo "-d delete the client and realm before re-creating them: default value is false"
+  echo "-r realm : default value test-realm"
+  echo "-c service account enabled client name : default value gatling"
+  echo "-d delete the client and realm : default value is false"
+  echo "-u user : default username user-0. The user's password will always be the username plus the suffix -password, ex: for user-0 this would be user-0-password"
   exit 1
 }
 
@@ -29,14 +30,26 @@ function create_realm {
   fi
 }
 
-function create_client_assign_roles {
+function create_service_enabled_client_assign_roles {
   #Create the client application with Service Account Roles setup
   CID=$(kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_ID -s enabled=true -i)
   echo "INFO: Created New ${CLIENT_ID} Client with Client ID ${CID}"
   #Update the client with necessary attributes
-  kcadm.sh update clients/$CID -r $REALM_NAME  -s "secret=setup-for-benchmark" -s serviceAccountsEnabled=true  -s publicClient=false -s 'redirectUris=["http://localhost:8081"]'
+  kcadm.sh update clients/$CID -r $REALM_NAME  -s "secret=setup-for-benchmark" -s serviceAccountsEnabled=true  -s publicClient=false -s 'redirectUris=["*"]'
   #Assign the necessary Service Account based roles to the client
   kcadm.sh add-roles -r $REALM_NAME --uusername service-account-$CLIENT_ID --cclientid realm-management --rolename manage-clients --rolename view-users --rolename manage-realm --rolename manage-users
+}
+
+function create_oidc_client {
+  #Create the client application with OIDC for Auth Code scenarios with confidential client secret 
+  CID=$(kcadm.sh create clients -r $REALM_NAME -s clientId=client-0 -s clientAuthenticatorType=client-secret -s secret=client-0-secret -s publicClient=false -s 'redirectUris=["*"]' -s serviceAccountsEnabled=true -s enabled=true -i)
+  echo "INFO: Created New client-0 Client"
+}
+
+function create_user {
+ kcadm.sh create users -s username=$USER_NAME -s enabled=true -r $REALM_NAME
+ kcadm.sh set-password -r $REALM_NAME --username $USER_NAME --new-password $USER_NAME-password
+ echo "INFO: Created New user ${USER_NAME} in ${REALM_NAME}" 
 }
 
 function delete_entities {
@@ -49,11 +62,12 @@ function delete_entities {
 #main()
 #setting default values
 OPTIND=1
-REALM_NAME=realm-0
+REALM_NAME=test-realm
 CLIENT_ID=gatling
 DELETE_ENTITIES='false'
+USER_NAME=user-0
 
-while getopts "k:r:c:d" arg; do
+while getopts "k:r:u:c:d" arg; do
   case $arg in
     k) KEYCLOAK_HOME="$OPTARG";
         ;;
@@ -62,6 +76,8 @@ while getopts "k:r:c:d" arg; do
     c) CLIENT_ID="$OPTARG";
         ;;
     d) DELETE_ENTITIES=true;
+        ;;
+    u) USER_NAME="$OPTARG";
         ;;
     \?) echo "ERROR: Invalid option: -${OPTARG}" >&2
         usage
@@ -89,4 +105,6 @@ else
 fi
 
 create_realm
-create_client_assign_roles
+create_service_enabled_client_assign_roles
+create_oidc_client
+create_user
