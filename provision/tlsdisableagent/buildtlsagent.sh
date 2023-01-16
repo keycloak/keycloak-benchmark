2cff37a94055d939c317fedf6fab2c93bd2a3502
Alexander Schwartz <aschwart@redhat.com>
2023-01-16 08:40:53 +0100

    Avoid concurrent download of maven wrapper
    Closes #285

--- provision/tlsdisableagent/buildtlsagent.sh
+++ provision/tlsdisableagent/buildtlsagent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 cd java-instrumentation-tool
-../../../mvnw clean package
+../../../mvnw clean package -B
 cd ..
 FILE=tlscheckdisable-agent.jar
 $JAVA_HOME/bin/java -jar java-instrumentation-tool/target/java-instrumentation-tool-*.jar \
