6de889d543e8ba259e8112e75998abdeb511a0b2
Lukas Hanusovsky <61745358+lhanusov@users.noreply.github.com>
2023-01-30 16:43:54 +0100

    dataset-import script refactoring (#280)

--- dataset/dataset-import.sh
+++ dataset/dataset-import.sh
@@ -1,92 +1,130 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 ### DATASET PROVIDER - KEYCLOAK REST API SERVICE ###
 
+set_environment_variables () {
+  ACTION="help"
+  REALM_COUNT="1"
+  REALM_NAME="realm-0"
+  CLIENTS_COUNT="100"
+  USERS_COUNT="100"
+  EVENTS_COUNT="100"
+  SESSIONS_COUNT="100"
+  HASH_ITERATIONS="20000"
+  KEYCLOAK_URI="https://keycloak.$(minikube ip).nip.io/realms/master/dataset"
+  REALM_PREFIX="realm"
+  STATUS_TIMEOUT="120"
+
+  while getopts ":a:r:n:c:u:e:o:i:p:l:t:" OPT
+  do
+    case $OPT in
+      a)
+        ACTION=$OPTARG
+        ;;
+      r)
+        REALM_COUNT=$OPTARG
+        ;;
+      n)
+        REALM_NAME=$OPTARG
+        ;;
+      c)
+        CLIENTS_COUNT=$OPTARG
+        ;;
+      u)
+        USERS_COUNT=$OPTARG
+        ;;
+      e)
+        EVENTS_COUNT=$OPTARG
+        ;;
+      o)
+        SESSIONS_COUNT=$OPTARG
+        ;;
+      i)
+        HASH_ITERATIONS=$OPTARG
+        ;;
+      p)
+        REALM_PREFIX=$OPTARG
+        ;;
+      l)
+        KEYCLOAK_URI=$OPTARG
+        ;;
+      t)
+        STATUS_TIMEOUT=$OPTARG
+        ;;
+      ?)
+        echo "Invalid option: $OPT, read the usage carefully -> "
+        help
+        exit 1
+    esac
+  done
+}
+
 create_realms () {
-  if [ $# -eq 0 ]
-  then
-    REALM_COUNT=1
-  else
-    REALM_COUNT=$1
-  fi
-  CLIENT_COUNT=${2:-100}
-  USER_COUNT=${3:-100}
-  PASSWORD_HASHING=${4:-20000}
-  KEYCLOAK_URI="${5:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
-
-  echo "Creating $REALM_COUNT realm/s with $CLIENT_COUNT client/s and $USER_COUNT user/s with $PASSWORD_HASHING password hash iterations."
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}create-realms?count=$REALM_COUNT&clients-per-realm=$CLIENT_COUNT&users-per-realm=$USER_COUNT&password-hash-iterations=$PASSWORD_HASHING"
-    echo ""
-
+  echo "Creating $1 realm/s with $2 client/s and $3 user/s with $4 password hash iterations."
+  execute_command "create-realms?count=$1&clients-per-realm=$2&users-per-realm=$3&password-hash-iterations=$4"
 }
 
 create_clients () {
-  KEYCLOAK_URI="${4:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Creating $1 client/s in realm $2"
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}create-clients?count=$1&realm-name=$2"
+  execute_command "create-clients?count=$1&realm-name=$2"
 }
 
 create_users () {
-  KEYCLOAK_URI="${4:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Creating $1 user/s in realm $2"
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}create-users?count=$1&realm-name=$2"
+  execute_command "create-users?count=$1&realm-name=$2"
 }
 
 create_events () {
-  KEYCLOAK_URI="${4:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Creating $1 event/s in realm $2"
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}create-events?count=$1&realm-name=$2"
+  execute_command "create-events?count=$1&realm-name=$2"
 }
 
 create_offline_sessions () {
-  KEYCLOAK_URI="${4:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Creating $1 offline sessions in realm $2"
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}create-offline-sessions?count=$1&realm-name=$2"
+  execute_command "create-offline-sessions?count=$1&realm-name=$2"
 }
 
 delete_realms () {
-  KEYCLOAK_URI="${3:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Deleting realm/s with prefix $1"
-  check_dataset_status
-  curl -ks "${KEYCLOAK_URI}remove-realms?remove-all=true&realm-prefix=$1"
+  execute_command "remove-realms?remove-all=true&realm-prefix=$1"
 }
 
-# shellcheck disable=SC2120
 dataset_provider_status () {
-  KEYCLOAK_URI="${1:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Dataset provider status"
-  curl -ks "${KEYCLOAK_URI}status"
-  echo ""
+  execute_command "status"
 }
 
 dataset_provider_status_completed () {
-  KEYCLOAK_URI="${2:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Dataset provider status of the last completed task"
   t=0
-  STATUS_TIMEOUT=${1:-120}
-  until curl -ks "${KEYCLOAK_URI}status-completed" | grep '"success":"true"' ; do
-    if [[ $t -gt $STATUS_TIMEOUT ]]
+  RESPONSE=$(execute_command "status-completed")
+  until [[ $(echo $RESPONSE | grep '"success":"true"') ]]
+  do
+    if [[ $t -gt $1 ]]
      then
-      echo "Status Polling timeout ${STATUS_TIMEOUT}s exceeded ";
+      echo "Status Polling timeout ${1}s exceeded ";
       break
     fi
     sleep 1 && ((t=t+1))
     echo "Polling...${t}s"
   done
+  echo $RESPONSE
 }
 
-# shellcheck disable=SC2120
 dataset_provider_clear_status_completed () {
-  KEYCLOAK_URI="${1:-"https://keycloak.$(minikube ip).nip.io"}/realms/master/dataset/"
   echo "Dataset provider clears the status of the last completed task"
-  curl -ks -X DELETE "${KEYCLOAK_URI}status-completed"
+  execute_command "status-completed" "-X DELETE"
+}
+
+execute_command () {
+  if [[ ! $1 =~ "status" ]]
+  then
+    check_dataset_status
+  fi
+  curl -ks $2 "${KEYCLOAK_URI}/$1"
   echo ""
 }
 
@@ -118,74 +156,69 @@ check_dataset_status () {
 }
 
 help () {
-  echo "Dataset import to the local minikube Keycloak application."
-  echo "Usage: "
-  echo "1) create realm/s with clients, users and password hash iterations - run it with or without arguments: -r | --create-realms '10 100 100 20000' 'https://keycloak.url.com'"
-  echo "2) create clients in specific realm: -c | --create-clients '100 realm-0' 'https://keycloak.url.com'"
-  echo "3) create users in specific realm: -u | --create-users '100 realm-0' 'https://keycloak.url.com'"
-  echo "4) create events in specific realm: -e | --create-events '100 realm-0' 'https://keycloak.url.com'"
-  echo "5) create offline sessions in specific realm: -o | --create-offline-sessions '100 realm-0' 'https://keycloak.url.com'"
-  echo "6) delete specific realm/s with prefix -d | --delete-realms realm 'https://keycloak.url.com'"
-  echo "7) dataset provider status : with optional args in this order <keycloak_url> -s | --status 'https://keycloak.url.com'"
-  echo "8) dataset provider status check of last completed job : with optional args in this order <timeout in seconds> <keycloak_url> | --status-completed 10 'https://keycloak.url.com' "
-  echo "9) dataset provider clear status of last completed job : with optional args in this order <keycloak_url> | --clear-status-completed 'https://keycloak.url.com'"
-  echo "10) dataset import script usage -h | --help"
+  echo "Dataset import to the local minikube Keycloak application - usage:"
+  echo "1) create realm/s with clients, users and password hash iterations - run -a (action) with or without other arguments: -a create-realms -r 10 -c 100 -u 100 -h 20000 -l 'https://keycloak.url.com'"
+  echo "2) create clients in specific realm: -a create-clients -c 100 -n realm-0 -l 'https://keycloak.url.com'"
+  echo "3) create users in specific realm: -a create-users -u 100 -n realm-0 -l 'https://keycloak.url.com'"
+  echo "4) create events in specific realm: -a create-events -e 100 -n realm-0 -l 'https://keycloak.url.com'"
+  echo "5) create offline sessions in specific realm: -a create-offline-sessions -o 100 -n realm-0' -l 'https://keycloak.url.com'"
+  echo "6) delete specific realm/s with prefix -a delete-realms -p realm -l 'https://keycloak.url.com'"
+  echo "7) dataset provider status -a status 'https://keycloak.url.com'"
+  echo "8) dataset provider status check of last completed job -a status-completed -t 10 -l 'https://keycloak.url.com'"
+  echo "9) dataset provider clear status of last completed job -a clear-status-completed -l 'https://keycloak.url.com'"
+  echo "10) dataset import script usage -a help"
 }
 
 main () {
-  SHORT_ARGS=r::,c:,u:,e:,o:,d:,s,h
-  LONG_ARGS=create-realms::,create-clients:,create-users:,create-events:,create-offline-sessions:,delete-realms:,help,status,status-completed,clear-status-completed
-  ARGS=$(getopt -a -n dataset-import -o ${SHORT_ARGS} -l ${LONG_ARGS} -- "$@")
+  set_environment_variables $@
 
-  eval set -- "$ARGS"
-
-  echo -n "[$1] "
-  case "$1" in
-    -r|--create-realms)
-      shift 2
-      # shellcheck disable=SC2068
-      create_realms ${@:2}
+  echo "Action: [$ACTION] "
+  case "$ACTION" in
+    create-realms)
+      create_realms $REALM_COUNT $CLIENTS_COUNT $USERS_COUNT $HASH_ITERATIONS
       exit 0
       ;;
-    -c|--create-clients)
-      create_clients $2 $3 $4
+    create-clients)
+      create_clients $CLIENTS_COUNT $REALM_NAME
       exit 0
       ;;
-    -u|--create-users)
-      create_users $2 $3 $4
+    create-users)
+      create_users $USERS_COUNT $REALM_NAME
       exit 0
       ;;
-    -e|--create-events)
-      create_events $2 $3 $4
+    create-events)
+      create_events $EVENTS_COUNT $REALM_NAME
       exit 0
       ;;
-    -o|--create-offline-sessions)
-      create_offline_sessions $2 $3 $4
+    create-offline-sessions)
+      create_offline_sessions $SESSIONS_COUNT $REALM_NAME
       exit 0
       ;;
-    -d|--delete-realms)
-      delete_realms $2 $3
+    delete-realms)
+      delete_realms $REALM_PREFIX
       exit 0
       ;;
-    -s|--status)
-      dataset_provider_status $3
+    status)
+      dataset_provider_status
       exit 0
       ;;
-    --status-completed)
-      dataset_provider_status_completed $3 $4
+    status-completed)
+      dataset_provider_status_completed $STATUS_TIMEOUT
       exit 0
       ;;
-    --clear-status-completed)
-      dataset_provider_clear_status_completed $3
+    clear-status-completed)
+      dataset_provider_clear_status_completed
       exit 0
       ;;
-    -h|--help)
+    help)
       help
       exit 0
       ;;
     *)
-      echo "Unknown option, use -h | --help to see permitted options."
+      echo "Action doesn't exist: $ACTION, read the usage carefully -> "
+      help
       exit 1
+      ;;
   esac
 }
 
