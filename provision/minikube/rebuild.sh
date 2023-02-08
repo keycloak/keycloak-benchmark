92d494f15b9a3b50555af37dc71dbd0144deec56
Alexander Schwartz <aschwart@redhat.com>
2023-02-08 08:50:53 +0100

    Enhancements and fixes to run custom Keycloak images

--- provision/minikube/rebuild.sh
+++ provision/minikube/rebuild.sh
@@ -8,13 +8,13 @@ if [ "$GITHUB_ACTIONS" == "" ]; then
   DRIVER=kvm2
   if [ "$(uname)" == "Darwin" ]; then
     DRIVER=hyperkit
-  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  elif [ "$(expr substr "$(uname -s)" 1 10)" == "MINGW64_NT" ]; then
     DRIVER=hyperv
   fi
   minikube config set driver ${DRIVER}
   minikube config set container-runtime docker
   # the version of Kubernetes needs to be in-sync with `provision-minikube.yml`
-  minikube start --container-runtime=docker --driver=${DRIVER} --docker-opt="default-ulimit=nofile=102400:102400" --kubernetes-version=v1.25.3
+  minikube start --disk-size=64GB --container-runtime=docker --driver=${DRIVER} --docker-opt="default-ulimit=nofile=102400:102400" --kubernetes-version=v1.25.3
 fi
 minikube addons enable ingress
 rm -rf .task
