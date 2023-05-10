cf37a747ff3c6dad9f77a127f0a0b2cd2fc3db0e
Tomas Kyjovsky <tkyjovsk@redhat.com>
2023-05-10 17:17:50 +0200

    ROSA provisioning scripts

---
+++ provision/aws/rosa_create_cluster.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+
+source ./.env
+
+AWS_ACCOUNT=${AWS_ACCOUNT:-$(aws sts get-caller-identity --query "Account" --output text)}
+if [ -z "$AWS_ACCOUNT" ]; then echo "Variable AWS_ACCOUNT needs to be set."; exit 1; fi
+
+if [ -z "$VERSION" ]; then echo "Variable VERSION needs to be set."; exit 1; fi
+CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
+if [ -z "$CLUSTER_NAME" ]; then echo "Variable CLUSTER_NAME needs to be set."; exit 1; fi
+if [ -z "$REGION" ]; then echo "Variable REGION needs to be set."; exit 1; fi
+if [ -z "$COMPUTE_MACHINE_TYPE" ]; then echo "Variable COMPUTE_MACHINE_TYPE needs to be set."; exit 1; fi
+
+if [ "$MULTI_AZ" = "true" ]; then MULTI_AZ_PARAM="--multi-az"; else MULTI_AZ_PARAM=""; fi
+if [ -z "$REPLICAS" ]; then echo "Variable REPLICAS needs to be set."; exit 1; fi
+
+echo "Checking if cluster ${CLUSTER_NAME} already exists."
+if rosa describe cluster --cluster="${CLUSTER_NAME}"; then
+  echo "Cluster ${CLUSTER_NAME} already exists. Exitting."
+else
+  echo "Verifying ROSA prerequisities."
+  echo "Check if AWS CLI is installed."; aws --version
+  echo "Check if ROSA CLI is installed."; rosa version
+  echo "Check if ELB service role is enabled."
+  if ! aws iam get-role --role-name "AWSServiceRoleForElasticLoadBalancing" --no-cli-pager; then
+    aws iam create-service-linked-role --aws-service-name "elasticloadbalancing.amazonaws.com"
+  fi
+  rosa whoami
+  rosa verify quota
+
+  echo "Installing ROSA cluster ${CLUSTER_NAME}"
+
+  ROSA_CMD="rosa create cluster \
+  --sts \
+  --cluster-name "${CLUSTER_NAME}" \
+  --version "${VERSION}" \
+  --role-arn arn:aws:iam::${AWS_ACCOUNT}:role/ManagedOpenShift-Installer-Role \
+  --support-role-arn arn:aws:iam::${AWS_ACCOUNT}:role/ManagedOpenShift-Support-Role \
+  --controlplane-iam-role arn:aws:iam::${AWS_ACCOUNT}:role/ManagedOpenShift-ControlPlane-Role \
+  --worker-iam-role arn:aws:iam::${AWS_ACCOUNT}:role/ManagedOpenShift-Worker-Role \
+  --operator-roles-prefix ${CLUSTER_NAME} \
+  --region ${REGION} ${MULTI_AZ_PARAM} \
+  --replicas ${REPLICAS} \
+  --compute-machine-type ${COMPUTE_MACHINE_TYPE} \
+  --machine-cidr 10.0.0.0/16 \
+  --service-cidr 172.30.0.0/16 \
+  --pod-cidr 10.128.0.0/14 \
+  --host-prefix 23"
+
+  echo $ROSA_CMD
+  $ROSA_CMD
+fi
+
+mkdir -p "logs/${CLUSTER_NAME}"
+
+echo "Creating operator roles."
+rosa create operator-roles --cluster "${CLUSTER_NAME}" --mode auto --yes > "logs/${CLUSTER_NAME}/$(date -uIs)_create-operator-roles.log"
+
+echo "Creating OIDC provider."
+rosa create oidc-provider --cluster "${CLUSTER_NAME}" --mode auto --yes > "logs/${CLUSTER_NAME}/$(date -uIs)_create-oidc-provider.log"
+
+echo "Waiting for cluster installation to finish."
+rosa logs install --cluster "${CLUSTER_NAME}" --watch --tail=1000000  > "logs/${CLUSTER_NAME}/$(date -uIs)_create-cluster.log"
+
+echo "Cluster installation complete."
+echo
+
+./rosa_recreate_admin.sh
---
+++ provision/aws/rosa_recreate_admin.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
+if [ -z "$CLUSTER_NAME" ]; then echo "Variable CLUSTER_NAME needs to be set."; exit 1; fi
+
+if [ -z "$ADMIN_PASSWORD" ]; then ADMIN_PASSWORD_PARAM=""; else ADMIN_PASSWORD_PARAM="--password $ADMIN_PASSWORD"; fi
+
+CLUSTER_DESCRIPTION=$(rosa describe cluster --cluster "$CLUSTER_NAME")
+
+CLUSTER_ID=$(echo "$CLUSTER_DESCRIPTION" | grep -oPm1 "^ID:\s*\K\w+")
+REGION=$(echo "$CLUSTER_DESCRIPTION" | grep -oPm1 "^Region:\s*\K[^\s]+")
+
+echo "CLUSTER_NAME: $CLUSTER_NAME"
+echo "CLUSTER_ID: $CLUSTER_ID"
+echo "REGION: $REGION"
+
+echo "Recreating the admin user."
+rosa delete admin --cluster "${CLUSTER_NAME}" --yes || true
+OC_LOGIN_CMD=$(rosa create admin --cluster "${CLUSTER_NAME}" $ADMIN_PASSWORD_PARAM | grep -o -m 1 "oc login.*")
+
+echo "New admin user created."
+echo
+echo "     $OC_LOGIN_CMD"
+echo
+ADMIN_PASSWORD=$(echo "$OC_LOGIN_CMD" | grep -oP "\-\-password\s+\K[^\s]+")
+
+echo "Recreating AWS secret"
+aws secretsmanager delete-secret --region $REGION --secret-id "${CLUSTER_ID}-cluster-admin" --no-cli-pager --force-delete-without-recovery || true
+echo "Waiting a bit for AWS secret deletion"
+sleep 1m
+
+aws secretsmanager create-secret --region $REGION --name "${CLUSTER_ID}-cluster-admin" --secret-string "$ADMIN_PASSWORD" --no-cli-pager
+#aws secretsmanager get-secret-value --region $REGION --secret-id "${CLUSTER_ID}-cluster-admin" --output json --no-cli-pager | jq .SecretString
+
+echo "Waiting a bit before attempting 'oc login'"
+sleep 1m
+
+echo "Waiting for 'oc login' to succeed."
+while ! $OC_LOGIN_CMD; do date -uIs; sleep 10s; done
