7361929cf88dabaf98bb1add7d581796f195ad65
Tomas Kyjovsky <tkyjovsk@redhat.com>
2023-05-10 17:17:50 +0200

    ROSA provisioning scripts

---
+++ provision/aws/rosa_delete_cluster.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+source ./.env
+
+CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
+if [ -z "$CLUSTER_NAME" ]; then echo "Variable CLUSTER_NAME needs to be set."; exit 1; fi
+
+CLUSTER_ID=$(rosa describe cluster --cluster "$CLUSTER_NAME" | grep -oPm1 "^ID:\s*\K\w+")
+echo "CLUSTER_ID: $CLUSTER_ID"
+
+rosa delete admin --cluster $CLUSTER_ID --yes || true
+
+rosa delete cluster --cluster $CLUSTER_ID --yes 
+
+mkdir -p "logs/${CLUSTER_NAME}"
+
+echo "Waiting for cluster uninstallation to finish."
+rosa logs uninstall --cluster $CLUSTER_ID --watch --tail=1000000 > "logs/${CLUSTER_NAME}/$(date -uIs)_delete-cluster.log"
+
+echo "Cluster uninstalled."
+
+rosa delete operator-roles --cluster $CLUSTER_ID --mode auto --yes > "logs/${CLUSTER_NAME}/$(date -uIs)_delete-operator-roles.log" || true
+rosa delete oidc-provider --cluster $CLUSTER_ID --mode auto --yes > "logs/${CLUSTER_NAME}/$(date -uIs)_delete-oidc-provider.log"
