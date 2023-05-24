57d88d3dde662d6b955324c6cfe927469ca306b4
Michal Hajas <mhajas@redhat.com>
2023-05-24 17:47:52 +0200

    Use the same password for all cluster-admins (#342)
    Closes #341

--- provision/aws/rosa_recreate_admin.sh
+++ provision/aws/rosa_recreate_admin.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
+source ./.env
+
 CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
 if [ -z "$CLUSTER_NAME" ]; then echo "Variable CLUSTER_NAME needs to be set."; exit 1; fi
 
-if [ -z "$ADMIN_PASSWORD" ]; then ADMIN_PASSWORD_PARAM=""; else ADMIN_PASSWORD_PARAM="--password $ADMIN_PASSWORD"; fi
+KEYCLOAK_MASTER_PASSWORD_SECRET_NAME=${KEYCLOAK_MASTER_PASSWORD_SECRET_NAME:-"keycloak-master-password"}
+# Force eu-central-1 region for secrets manager so we all work with the same secret
+SECRET_MANAGER_REGION="eu-central-1"
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-$(aws secretsmanager get-secret-value --region $SECRET_MANAGER_REGION --secret-id $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME --query SecretString --output text --no-cli-pager)}
+
+if [ -z "$ADMIN_PASSWORD" ]; then
+  ./aws_rotate_keycloak_master_password.sh
+  ADMIN_PASSWORD=$(aws secretsmanager get-secret-value --region $SECRET_MANAGER_REGION --secret-id $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME --query SecretString --output text --no-cli-pager)
+fi
 
 CLUSTER_DESCRIPTION=$(rosa describe cluster --cluster "$CLUSTER_NAME")
 
@@ -17,21 +27,12 @@ echo "REGION: $REGION"
 
 echo "Recreating the admin user."
 rosa delete admin --cluster "${CLUSTER_NAME}" --yes || true
-OC_LOGIN_CMD=$(rosa create admin --cluster "${CLUSTER_NAME}" $ADMIN_PASSWORD_PARAM | grep -o -m 1 "oc login.*")
+OC_LOGIN_CMD=$(rosa create admin --cluster "${CLUSTER_NAME}" --password "$ADMIN_PASSWORD" | grep -o -m 1 "oc login.*")
 
 echo "New admin user created."
 echo
 echo "     $OC_LOGIN_CMD"
 echo
-ADMIN_PASSWORD=$(echo "$OC_LOGIN_CMD" | grep -oP "\-\-password\s+\K[^\s]+")
-
-echo "Recreating AWS secret"
-aws secretsmanager delete-secret --region $REGION --secret-id "${CLUSTER_ID}-cluster-admin" --no-cli-pager --force-delete-without-recovery || true
-echo "Waiting a bit for AWS secret deletion"
-sleep 1m
-
-aws secretsmanager create-secret --region $REGION --name "${CLUSTER_ID}-cluster-admin" --secret-string "$ADMIN_PASSWORD" --no-cli-pager
-#aws secretsmanager get-secret-value --region $REGION --secret-id "${CLUSTER_ID}-cluster-admin" --output json --no-cli-pager | jq .SecretString
 
 echo "Waiting a bit before attempting 'oc login'"
 sleep 1m
