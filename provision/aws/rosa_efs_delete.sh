45df0507c452300facc75c0579d0b2291e58dc29
Alexander Schwartz <aschwart@redhat.com>
2023-06-26 11:54:28 +0200

    Support EFS on ROSA for ReadWriteMany PVCs (#387)
    Closes #386

---
+++ provision/aws/rosa_efs_delete.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# don't use 'set -e' here, as we want to cleanup also half-installed EFS setups
+
+if [ -f ./.env ]; then
+  source ./.env
+fi
+
+export AWS_REGION=${REGION}
+
+EFS=$(oc get sc/efs-sc -o jsonpath='{.parameters.fileSystemId}')
+
+for MOUNT_TARGET in $(aws efs describe-mount-targets \
+  --region=$AWS_REGION \
+  --file-system-id=$EFS \
+  --output json \
+  | jq -r '.MountTargets[].MountTargetId'); do
+  aws efs delete-mount-target --mount-target-id $MOUNT_TARGET --region $AWS_REGION
+done
+
+while true; do
+    LIFECYCLE_STATE="$(aws efs describe-mount-targets \
+                       --region=$AWS_REGION \
+                       --file-system-id=$EFS \
+                       --output json \
+                       | jq -r '.MountTargets[].MountTargetId')"
+    if [[ "${LIFECYCLE_STATE}" == "" ]]; then break; fi
+    sleep 1
+    echo -n '.'
+done
+
+aws efs delete-file-system --file-system-id $EFS --region $AWS_REGION
+
+NODE=$(oc get nodes --selector=node-role.kubernetes.io/worker \
+  -o jsonpath='{.items[0].metadata.name}')
+VPC=$(aws ec2 describe-instances \
+  --filters "Name=private-dns-name,Values=$NODE" \
+  --output json \
+  --query 'Reservations[*].Instances[*].{VpcId:VpcId}' \
+  --region $AWS_REGION \
+  | jq -r '.[0][0].VpcId')
+CIDR=$(aws ec2 describe-vpcs \
+  --filters "Name=vpc-id,Values=$VPC" \
+  --query 'Vpcs[*].CidrBlock' \
+  --output json \
+  --region $AWS_REGION \
+  | jq -r '.[0]')
+SG=$(aws ec2 describe-instances --filters \
+  "Name=private-dns-name,Values=$NODE" \
+  --query 'Reservations[*].Instances[*].{SecurityGroups:SecurityGroups}' \
+  --output json \
+  --region $AWS_REGION \
+  | jq -r '.[0][0].SecurityGroups[0].GroupId')
+echo "CIDR - $CIDR,  SG - $SG"
+
+aws ec2 revoke-security-group-ingress \
+ --group-id $SG \
+ --protocol tcp \
+ --region $AWS_REGION \
+ --port 2049 \
+ --cidr $CIDR
+
+cd efs
+
+CCO_POD_NAME=$(oc get po -n openshift-cloud-credential-operator -l app=cloud-credential-operator -o jsonpath='{.items[*].metadata.name}')
+
+oc cp -c cloud-credential-operator openshift-cloud-credential-operator/${CCO_POD_NAME}:/usr/bin/ccoctl ./ccoctl --retries=999
+
+chmod 775 ./ccoctl
+
+./ccoctl aws delete --name=${CLUSTER_NAME} --region=${AWS_REGION}
+
+oc delete storageclass efs-sc
+
+oc delete -n openshift-cluster-csi-drivers Subscription aws-efs-csi-driver-operator
+
+oc delete -n openshift-cluster-csi-drivers Secret aws-efs-cloud-credentials
+
+oc delete ClusterCSIDriver efs.csi.aws.com
+
+for OPERATOR_GROUP in $(oc get -n openshift-cluster-csi-drivers OperatorGroup -o name); do
+  oc delete -n openshift-cluster-csi-drivers $OPERATOR_GROUP
+done
