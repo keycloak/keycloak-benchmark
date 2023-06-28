67f7dd610db8c71d7228b682fde23e9b074290a2
Alexander Schwartz <aschwart@redhat.com>
2023-06-28 16:29:50 +0200

    Adding a machinepool for load tests

--- provision/aws/rosa_create_cluster.sh
+++ provision/aws/rosa_create_cluster.sh
@@ -87,4 +87,6 @@ echo
 
 ./rosa_recreate_admin.sh
 
+rosa create machinepool -c "${CLUSTER_NAME}" --instance-type m5.4xlarge --max-replicas 0 --min-replicas 0 --name scaling --use-spot-instances --enable-autoscaling
+
 ./rosa_efs_create.sh
--- provision/openshift/isup.sh
+++ provision/openshift/isup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # set -x
 
+set -e
+
 # Default values for variables from Taskfile.yml are not part of .env file, therefore we need to load them manually
 KC_NAMESPACE_PREFIX=$(cat .task/var-KC_NAMESPACE_PREFIX)
 KC_HOSTNAME_SUFFIX=$(cat .task/var-KC_HOSTNAME_SUFFIX)
@@ -10,9 +12,9 @@ if [ -f ./.env ]; then
 fi
 
 # kill all CrashLoopBackOff and ImagePullBackOff pods to trigger a fast restart and not wait Kubernetes
-kubectl get pods -n "${KC_NAMESPACE_PREFIX}keycloak" | grep -E "(BackOff|Error)" | tr -s " " | cut -d" " -f1 | xargs -r -L 1 kubectl delete pod -n keycloak
+kubectl get pods -n "${KC_NAMESPACE_PREFIX}keycloak" | grep -E "(BackOff|Error)" | tr -s " " | cut -d" " -f1 | xargs -r -L 1 kubectl delete pod -n ${KC_NAMESPACE_PREFIX}keycloak
 
-MAXRETRIES=600
+MAXRETRIES=1200
 
 declare -A SERVICES=( \
  ["keycloak-${KC_NAMESPACE_PREFIX}keycloak.${KC_HOSTNAME_SUFFIX}"]="realms/master/.well-known/openid-configuration" \
@@ -24,8 +26,8 @@ for SERVICE in "${!SERVICES[@]}"; do
 
   if [ "${SERVICE}" == "keycloak-${KC_NAMESPACE_PREFIX}keycloak.${KC_HOSTNAME_SUFFIX}" ]
   then
-    kubectl wait --for=condition=Available --timeout=300s deployments.apps/keycloak-operator -n "${KC_NAMESPACE_PREFIX}keycloak"
-    kubectl wait --for=condition=Ready --timeout=300s keycloaks.k8s.keycloak.org/keycloak -n "${KC_NAMESPACE_PREFIX}keycloak"
+    kubectl wait --for=condition=Available --timeout=1200s deployments.apps/keycloak-operator -n "${KC_NAMESPACE_PREFIX}keycloak"
+    kubectl wait --for=condition=Ready --timeout=1200s keycloaks.k8s.keycloak.org/keycloak -n "${KC_NAMESPACE_PREFIX}keycloak"
   fi
 
   until kubectl get ingress -A 2>/dev/null | grep ${SERVICE} >/dev/null && curl -k -f -v https://${SERVICE}/${SERVICES[${SERVICE}]} >/dev/null 2>/dev/null
