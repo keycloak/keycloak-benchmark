c63529dd0fe0e64f3f942729ca3353f1e7b8fb66
Ryan Emerson <remerson@redhat.com>
2023-07-25 16:27:26 +0100

    Add mechanism to preserve seleect Aurora DB clusters overnight (#451)

--- provision/aws/rds/aurora_cluster_reaper.sh
+++ provision/aws/rds/aurora_cluster_reaper.sh
@@ -4,6 +4,11 @@ if [[ "$RUNNER_DEBUG" == "1" ]]; then
   set -x
 fi
 
+# Removes all Aurora DB clusters that are not tagged with the key "keepalive"
+# To tag an Aurora Cluster with "keepalive" execute:
+# `aws rds add-tags-to-resource --resource-name <arn> --tags Key=keepalive --region <region>`
+# Where the `<arn>` is the arn of the DB cluster
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/aurora_common.sh
 
@@ -24,7 +29,18 @@ for REGION in ${REGIONS}; do
         echo ${DB_INSTANCES} | jq -c '.[]' | while read i; do
             export AURORA_CLUSTER=$(echo $i | jq -r .DBClusterIdentifier)
             export AURORA_INSTANCE=$(echo $i | jq -r .DBInstanceIdentifier)
-            sh ${SCRIPT_DIR}/aurora_delete.sh
+
+            KEEP_ALIVE=$(aws rds describe-db-clusters \
+                --region ${REGION} \
+                --db-cluster-identifier ${AURORA_CLUSTER} \
+                --query DBClusters[*].TagList[?key=='keepalive'] \
+                --output json \
+                | jq -c '.[]' \
+                | jq length
+            )
+            if [ ${KEEP_ALIVE} == "0" ]; then
+                sh ${SCRIPT_DIR}/aurora_delete.sh
+            fi
         done
     fi
 done
