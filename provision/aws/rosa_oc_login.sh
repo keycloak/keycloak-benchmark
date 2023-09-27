370e9155fb2c7dd97d2c656bd9cfdbd87c92d7da
Alexander Schwartz <aschwart@redhat.com>
2023-09-27 08:57:57 +0200

    prevent the admin command to be logged in debug mode

--- provision/aws/rosa_oc_login.sh
+++ provision/aws/rosa_oc_login.sh
@@ -15,9 +15,16 @@ KEYCLOAK_MASTER_PASSWORD_SECRET_NAME=${KEYCLOAK_MASTER_PASSWORD_SECRET_NAME:-"ke
 SECRET_MANAGER_REGION="eu-central-1"
 
 API_URL=$(rosa describe cluster -c "$CLUSTER_NAME" -o json | jq -r '.api.url')
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set +x
+fi
 ADMIN_PASSWORD=$(aws secretsmanager get-secret-value --region $SECRET_MANAGER_REGION --secret-id $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME --query SecretString --output text --no-cli-pager)
 if [ "$GITHUB_ACTIONS" != "" ]; then
   echo "::add-mask::${ADMIN_PASSWORD}"
+fi
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
 fi
 
 oc login $API_URL --username cluster-admin --password $ADMIN_PASSWORD --insecure-skip-tls-verify
