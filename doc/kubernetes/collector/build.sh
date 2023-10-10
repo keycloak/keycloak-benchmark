ce6f8944c485b40e457453d5e11a6fc4f10a7979
Kamesh Akella <kamesh.asp@gmail.com>
2023-10-10 17:34:42 -0400

    draft how-to

--- doc/kubernetes/collector/build.sh
+++ doc/kubernetes/collector/build.sh
@@ -24,6 +24,30 @@ helm template --debug ${STARTDIR}/../../../provision/minikube/keycloak \
   --set dbPoolInitialSize=30 \
   --set dbPoolMaxSize=30 \
   --set dbPoolMinSize=30 \
-  --set jvmDebug=false \
   | yq \
   > ${BUILDDIR}/helm/keycloak.yaml
+
+# Those value match the Keycloak on ROSA Benchmark Key Results example
+helm template --debug ${STARTDIR}/../../../provision/minikube/keycloak \
+  --set hostname=minikube.nip.io \
+  --set jvmdebug=false \
+  --set cryostat=false \
+  --set instances=3 \
+  --set cpuRequests=2 \
+  --set cpuLimits=6 \
+  --set memoryRequestsMB=1250 \
+  --set memoryLimitsMB=2250 \
+  --set heapInitMB=512 \
+  --set heapMaxMB=1524 \
+  --set dbPoolInitialSize=30 \
+  --set dbPoolMaxSize=30 \
+  --set dbPoolMinSize=30 \
+  --set infinispan.customConfig=true \
+  --set infinispan.configFile=config/kcb-infinispan-cache-remote-store-config.xml \
+  --set infinispan.remoteStore.enabled=true \
+  --set infinispan.remoteStore.host=infinispan.keycloak.svc \
+  --set infinispan.remoteStore.password=secure_password \
+  --set infinispan.site=keycloak \
+  | yq \
+  > ${BUILDDIR}/helm/keycloak-ispn.yaml
+
