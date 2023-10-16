931e2dc2826bf802d17466004a94edc61f767df9
Pedro Ruivo <pruivo@users.noreply.github.com>
2023-10-16 16:28:07 +0100

    Docs: deploying Infinispan with Operator (#569)

--- doc/kubernetes/collector/build.sh
+++ doc/kubernetes/collector/build.sh
@@ -43,3 +43,56 @@ helm template --debug ${STARTDIR}/../../../provision/minikube/keycloak \
   | yq \
   > ${BUILDDIR}/helm/keycloak-ispn.yaml
 
+
+# Infinispan single cluster
+helm template --debug ${STARTDIR}/../../../provision/infinispan/ispn-helm \
+  --set namespace=ispn-namespace \
+  --set replicas=3 \
+  --set crossdc.enabled=false \
+  --set metrics.histograms=false \
+  --set hotrodPassword="strong-password" \
+  --set cacheDefaults.crossSiteMode=ASYNC \
+  --set cacheDefaults.stateTransferMode=AUTO \
+  > ${BUILDDIR}/helm/ispn-single.yaml
+
+# Infinispan site A deployment
+helm template --debug ${STARTDIR}/../../../provision/infinispan/ispn-helm \
+  --set namespace=ispn-namespace \
+  --set replicas=3 \
+  --set crossdc.enabled=true \
+  --set crossdc.local.name=site-a \
+  --set crossdc.local.gossipRouterEnabled=true \
+  --set crossdc.remote.name=site-b \
+  --set crossdc.remote.gossipRouterEnabled=true \
+  --set crossdc.remote.namespace=ispn-namespace \
+  --set crossdc.remote.url=openshift://api.site-b \
+  --set crossdc.remote.secret=xsite-token-secret \
+  --set crossdc.route.enabled=true \
+  --set crossdc.route.tls.keystore.secret=xsite-keystore-secret \
+  --set crossdc.route.tls.truststore.secret=xsite-truststore-secret \
+  --set metrics.histograms=false \
+  --set hotrodPassword="strong-password" \
+  --set cacheDefaults.crossSiteMode=ASYNC \
+  --set cacheDefaults.stateTransferMode=AUTO \
+  > ${BUILDDIR}/helm/ispn-site-a.yaml
+
+# Infinispan site B deployment
+helm template --debug ${STARTDIR}/../../../provision/infinispan/ispn-helm \
+  --set namespace=ispn-namespace \
+  --set replicas=3 \
+  --set crossdc.enabled=true \
+  --set crossdc.local.name=site-b \
+  --set crossdc.local.gossipRouterEnabled=true \
+  --set crossdc.remote.name=site-a \
+  --set crossdc.remote.gossipRouterEnabled=true \
+  --set crossdc.remote.namespace=ispn-namespace \
+  --set crossdc.remote.url=openshift://api.site-a \
+  --set crossdc.remote.secret=xsite-token-secret \
+  --set crossdc.route.enabled=true \
+  --set crossdc.route.tls.keystore.secret=xsite-keystore-secret \
+  --set crossdc.route.tls.truststore.secret=xsite-truststore-secret \
+  --set metrics.histograms=false \
+  --set hotrodPassword="strong-password" \
+  --set cacheDefaults.crossSiteMode=ASYNC \
+  --set cacheDefaults.stateTransferMode=AUTO \
+  > ${BUILDDIR}/helm/ispn-site-b.yaml
