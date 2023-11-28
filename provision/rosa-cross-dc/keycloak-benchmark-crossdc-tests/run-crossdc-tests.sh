47f6dcab0f9f3e4b91f8a270d24dbb0eeb905b26
Kamesh Akella <kamesh.asp@gmail.com>
2023-11-27 23:52:09 -0500

    Stretch attempt to add an GHA to run the CrossDC functional tests
    Signed-off-by: Kamesh Akella <kamesh.asp@gmail.com>

---
+++ provision/rosa-cross-dc/keycloak-benchmark-crossdc-tests/run-crossdc-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
+fi
+
+CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
+KEYCLOAK_MASTER_PASSWORD_SECRET_NAME=${KEYCLOAK_MASTER_PASSWORD_SECRET_NAME:-"keycloak-master-password"}
+# Force eu-central-1 region for secrets manager so we all work with the same secret
+SECRET_MANAGER_REGION="eu-central-1"
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  # prevent logging the password in debug mode
+  set +x
+fi
+ISPN_PASSWORD=$(aws secretsmanager get-secret-value --region $SECRET_MANAGER_REGION --secret-id $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME --query SecretString --output text --no-cli-pager)
+if [ "$GITHUB_ACTIONS" != "" ]; then
+  echo "::add-mask::${ISPN_PASSWORD}"
+fi
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
+fi
+
+./mvnw -B clean install -DcrossDCTests \
+-Dinfinispan.dc1.url=$ISPN_DC1_URL -Dkeycloak.dc1.url=$KEYCLOAK_DC1_URL \
+-Dinfinispan.dc2.url=$ISPN_DC2_URL -Dkeycloak.dc2.url=$KEYCLOAK_DC2_URL \
+-Dinfinispan.password=$ISPN_PASSWORD
