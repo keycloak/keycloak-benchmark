5043aa5af14e1c08e506aff78c273f36531a73c3
Martin Kanis <mkanis@redhat.com>
2023-12-05 14:11:39 +0100

    Mask passwords in GHA when executing scripts via go-task
    Signed-off-by: Martin Kanis <mkanis@redhat.com>

--- provision/rosa-cross-dc/keycloak-benchmark-crossdc-tests/run-crossdc-tests.sh
+++ provision/rosa-cross-dc/keycloak-benchmark-crossdc-tests/run-crossdc-tests.sh
@@ -9,17 +9,7 @@ KEYCLOAK_MASTER_PASSWORD_SECRET_NAME=${KEYCLOAK_MASTER_PASSWORD_SECRET_NAME:-"ke
 # Force eu-central-1 region for secrets manager so we all work with the same secret
 SECRET_MANAGER_REGION="eu-central-1"
 
-if [[ "$RUNNER_DEBUG" == "1" ]]; then
-  # prevent logging the password in debug mode
-  set +x
-fi
 ISPN_PASSWORD=$(aws secretsmanager get-secret-value --region $SECRET_MANAGER_REGION --secret-id $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME --query SecretString --output text --no-cli-pager)
-if [ "$GITHUB_ACTIONS" != "" ]; then
-  echo "::add-mask::${ISPN_PASSWORD}"
-fi
-if [[ "$RUNNER_DEBUG" == "1" ]]; then
-  set -x
-fi
 
 ./mvnw -B -f provision/rosa-cross-dc/keycloak-benchmark-crossdc-tests/pom.xml clean install -DcrossDCTests \
 -Dinfinispan.dc1.url=$ISPN_DC1_URL -Dkeycloak.dc1.url=$KEYCLOAK_DC1_URL \
