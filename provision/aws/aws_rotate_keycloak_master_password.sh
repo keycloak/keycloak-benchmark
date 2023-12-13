ade546f3312aa692aa08005d345d47fe95edefac
Martin Kanis <martin-kanis@users.noreply.github.com>
2023-12-13 16:14:06 +0100

    Fix rotate password script (#630)
    Signed-off-by: Martin Kanis <mkanis@redhat.com>

--- provision/aws/aws_rotate_keycloak_master_password.sh
+++ provision/aws/aws_rotate_keycloak_master_password.sh
@@ -10,7 +10,7 @@ KEYCLOAK_MASTER_PASSWORD_SECRET_NAME=${KEYCLOAK_MASTER_PASSWORD_SECRET_NAME:-"ke
 REGION="eu-central-1"
 
 mkdir -p "logs"
-if aws secretsmanager describe-secret --region $REGION --secret-id "$KEYCLOAK_MASTER_PASSWORD_SECRET_NAME" --no-cli-pager > "logs/${CLUSTER_NAME}/$(date -uIs)_describe-secret.log"; then
+if aws secretsmanager describe-secret --region $REGION --secret-id "$KEYCLOAK_MASTER_PASSWORD_SECRET_NAME" --no-cli-pager > "logs/$(date -uIs)_describe-secret.log"; then
   echo "Secret $KEYCLOAK_MASTER_PASSWORD_SECRET_NAME already exists. Updating..."
   aws secretsmanager update-secret --region $REGION --secret-id "$KEYCLOAK_MASTER_PASSWORD_SECRET_NAME" --secret-string "$KEYCLOAK_MASTER_PASSWORD" --no-cli-pager > "logs/$(date -uIs)_update-secret.log"
 else
