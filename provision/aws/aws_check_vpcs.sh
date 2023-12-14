dde6cdfed7794ea809e1ee62589fc73b4d299f18
Michal Hajas <mhajas@redhat.com>
2023-12-14 14:21:28 +0100

    Print dangling VPCs every day (#631)
    Print dangling VPCs every day
    
    Signed-off-by: Michal Hajas <mhajas@redhat.com>

---
+++ provision/aws/aws_check_vpcs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
+fi
+
+EXIT_CODE=0
+
+for r in $(aws account list-regions --query 'Regions[?RegionOptStatus != `DISABLED`].RegionName' --output text);
+do
+  VPCS_JSON=$(aws ec2 describe-vpcs --region "$r" --query 'Vpcs[?IsDefault == `false`]' --output json --no-cli-pager 2>/dev/null)
+
+  VPC_IDS=( $(echo $VPCS_JSON | jq -r '.[].VpcId') )
+  if [ ${#VPC_IDS[@]} -gt 0 ]; then
+    echo "$r region contains VPCs that were not cleaned up [${#VPC_IDS[@]}]"
+    echo "--------------------------------------------------------------------"
+    for VPC_ID in "${VPC_IDS[@]}"; do
+    echo "---------------------"
+      echo $VPCS_JSON | jq ".[] | select(.VpcId == \"$VPC_ID\")" | yq -P
+    echo "---------------------"
+    done
+    echo "--------------------------------------------------------------------"
+    EXIT_CODE=1
+  fi
+done
+
+exit $EXIT_CODE
+
