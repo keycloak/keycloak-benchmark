47570f83fe50cf216f3cbb7e422a6105fdde7547
Alexander Schwartz <aschwart@redhat.com>
2024-05-06 15:29:34 +0200

    Make ROSA cluster creation idempotent even if CIDRs change
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- provision/aws/rosa_create_cluster.sh
+++ provision/aws/rosa_create_cluster.sh
@@ -26,47 +26,53 @@ requiredEnv AWS_ACCOUNT CLUSTER_NAME REGION CIDR
 
 export CLUSTER_NAME=${CLUSTER_NAME:-$(whoami)}
 
-echo "Verifying ROSA prerequisites."
-echo "Check if AWS CLI is installed."; aws --version
-echo "Check if ROSA CLI is installed."; rosa version
-echo "Check if ELB service role is enabled."
-if ! aws iam get-role --role-name "AWSServiceRoleForElasticLoadBalancing" --no-cli-pager; then
-  aws iam create-service-linked-role --aws-service-name "elasticloadbalancing.amazonaws.com"
+echo "Checking if cluster ${CLUSTER_NAME} already exists."
+if rosa describe cluster --cluster="${CLUSTER_NAME}"; then
+  echo "Cluster ${CLUSTER_NAME} already exists."
+else
+  echo "Verifying ROSA prerequisites."
+  echo "Check if AWS CLI is installed."; aws --version
+  echo "Check if ROSA CLI is installed."; rosa version
+  echo "Check if ELB service role is enabled."
+  if ! aws iam get-role --role-name "AWSServiceRoleForElasticLoadBalancing" --no-cli-pager; then
+    aws iam create-service-linked-role --aws-service-name "elasticloadbalancing.amazonaws.com"
+  fi
+  rosa whoami
+  rosa verify quota
+
+  echo "Installing ROSA cluster ${CLUSTER_NAME}"
+
+  cd ${SCRIPT_DIR}/../opentofu/modules/rosa/hcp
+  tofu init
+  WORKSPACE=${CLUSTER_NAME}-${REGION}
+  tofu workspace new ${WORKSPACE} || true
+  export TF_WORKSPACE=${WORKSPACE}
+
+  AVAILABILITY_ZONES=${AVAILABILITY_ZONES:-"${REGION}a"}
+
+  TOFU_CMD="tofu apply -auto-approve \
+    -var vpc_cidr=${CIDR} \
+    -var availability_zones=${AVAILABILITY_ZONES} \
+    -var cluster_name=${CLUSTER_NAME} \
+    -var region=${REGION} \
+    -var subnet_cidr_prefix=28"
+
+  if [ -n "${COMPUTE_MACHINE_TYPE}" ]; then
+    TOFU_CMD+=" -var instance_type=${COMPUTE_MACHINE_TYPE}"
+  fi
+
+  if [ -n "${VERSION}" ]; then
+    TOFU_CMD+=" -var openshift_version=${VERSION}"
+  fi
+
+  if [ -n "${REPLICAS}" ]; then
+    TOFU_CMD+=" -var replicas=${REPLICAS}"
+  fi
+
+  echo ${TOFU_CMD}
+  ${TOFU_CMD}
+
 fi
-rosa whoami
-rosa verify quota
-
-echo "Installing ROSA cluster ${CLUSTER_NAME}"
-
-cd ${SCRIPT_DIR}/../opentofu/modules/rosa/hcp
-tofu init
-WORKSPACE=${CLUSTER_NAME}-${REGION}
-tofu workspace new ${WORKSPACE} || true
-export TF_WORKSPACE=${WORKSPACE}
-
-AVAILABILITY_ZONES=${AVAILABILITY_ZONES:-"${REGION}a"}
-
-TOFU_CMD="tofu apply -auto-approve \
-  -var vpc_cidr=${CIDR} \
-  -var availability_zones=${AVAILABILITY_ZONES} \
-  -var cluster_name=${CLUSTER_NAME} \
-  -var region=${REGION} \
-  -var subnet_cidr_prefix=28"
-
-if [ -n "${COMPUTE_MACHINE_TYPE}" ]; then
-  TOFU_CMD+=" -var instance_type=${COMPUTE_MACHINE_TYPE}"
-fi
-
-if [ -n "${VERSION}" ]; then
-  TOFU_CMD+=" -var openshift_version=${VERSION}"
-fi
-
-if [ -n "${REPLICAS}" ]; then
-  TOFU_CMD+=" -var replicas=${REPLICAS}"
-fi
-
-echo ${TOFU_CMD}
-${TOFU_CMD}
 
 SCALING_MACHINE_POOL=$(rosa list machinepools -c "${CLUSTER_NAME}" -o json | jq -r '.[] | select(.id == "scaling") | .id')
 if [[ "${SCALING_MACHINE_POOL}" != "scaling" ]]; then
