bb3653b2577a0a642b78c069b2597860abf866f0
Alexander Schwartz <aschwart@redhat.com>
2024-08-20 11:52:00 +0200

    Prevent login page cookie issues with a descriptive error message
    Related to #945
    
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- benchmark/src/main/scala/keycloak/scenario/authentication/LoginUserPassword.scala
+++ benchmark/src/main/scala/keycloak/scenario/authentication/LoginUserPassword.scala
@@ -1,8 +1,11 @@
 package keycloak.scenario.authentication
 
 import keycloak.scenario.{CommonSimulation, KeycloakScenarioBuilder}
+import org.keycloak.benchmark.Config
 
 class LoginUserPassword extends CommonSimulation {
+
+  Config.preventLocalhostServerUris()
 
   setUp("Authentication - Login Username/Password", new KeycloakScenarioBuilder()
     .openLoginPage(true)
