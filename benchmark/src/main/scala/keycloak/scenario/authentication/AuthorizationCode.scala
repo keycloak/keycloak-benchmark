a5a8dafeadfb05f6ac0c86b60b195dfa2eab5d4e
Alexander Schwartz <aschwart@redhat.com>
2024-10-14 18:32:13 +0200

    Reapply "Prevent login page cookie issues with a descriptive error message"
    This reverts commit 9fa84b392f517dbef68b82fe1dd66c101307378f.

--- benchmark/src/main/scala/keycloak/scenario/authentication/AuthorizationCode.scala
+++ benchmark/src/main/scala/keycloak/scenario/authentication/AuthorizationCode.scala
@@ -1,8 +1,11 @@
 package keycloak.scenario.authentication
 
 import keycloak.scenario.{CommonSimulation, KeycloakScenarioBuilder}
+import org.keycloak.benchmark.Config
 
 class AuthorizationCode extends CommonSimulation {
+
+  Config.preventLocalhostServerUris()
 
   setUp("Authentication - Authorization Code Username/Password", new KeycloakScenarioBuilder()
     .openLoginPage(true)
