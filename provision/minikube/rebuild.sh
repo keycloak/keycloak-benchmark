5061f0e096b4fb7c60e78012fa5a8c60087df348
Pedro Ruivo <pruivo@users.noreply.github.com>
2024-12-06 18:34:04 +0000

    Enable NetworkPolicy by default
    Closes #1072
    
    Signed-off-by: Pedro Ruivo <pruivo@redhat.com>
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>
    Co-authored-by: Alexander Schwartz <aschwart@redhat.com>

--- provision/minikube/rebuild.sh
+++ provision/minikube/rebuild.sh
@@ -38,8 +38,7 @@ if [ "$GITHUB_ACTIONS" == "" ]; then
   minikube config set driver ${DRIVER}
   minikube config set container-runtime docker
   # the version of Kubernetes needs to be in-sync with `provision-minikube.yml`
-  minikube start --disk-size=64GB --container-runtime=docker --driver=${DRIVER} --docker-opt="default-ulimit=nofile=102400:102400" --kubernetes-version=v1.25.3
+  minikube start --addons=ingress --disk-size=64GB --container-runtime=docker --driver=${DRIVER} --docker-opt="default-ulimit=nofile=102400:102400" --kubernetes-version=v1.27.10 --cni cilium
 fi
-minikube addons enable ingress
 rm -rf .task
 echo "Minikube initialized. Now run 'task' to provision it with Keycloak"
