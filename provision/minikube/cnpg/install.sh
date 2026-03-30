4c3fe01407c18886a5ce01300c99b33de2c5ed1b
Tomas Kyjovsky <tkyjovsk@ibm.com>
2026-03-30 15:42:07 +0200

    Single-line secret creation for CNPG JDBC connection, to make it consistent with Keycloak docs.
    Signed-off-by: Tomas Kyjovsky <tkyjovsk@ibm.com>

--- provision/minikube/cnpg/install.sh
+++ provision/minikube/cnpg/install.sh
@@ -43,15 +43,20 @@ kubectl create ns $KEYCLOAK_NAMESPACE || true
 if kubectl -n $KEYCLOAK_NAMESPACE get secret keycloak-db-secret; then
   kubectl -n $KEYCLOAK_NAMESPACE delete secret keycloak-db-secret
 fi
-secret=$(kubectl get -n $CNPG_NAMESPACE secret cnpg-keycloak-app -ojson)
-u=$(echo "$secret" | jq -r .data.username | base64 -d)
-p=$(echo "$secret" | jq -r .data.password | base64 -d)
-kubectl -n $KEYCLOAK_NAMESPACE create secret generic keycloak-db-secret --from-literal="username=$u" --from-literal="password=$p"
+kubectl get secret cnpg-keycloak-app --namespace $CNPG_NAMESPACE -o go-template='
+apiVersion: v1
+kind: Secret
+metadata:
+  name: keycloak-db-secret
+type: Opaque
+data:
+  username: {{ .data.username }}
+  password: {{ .data.password }}
+' | kubectl apply -n $KEYCLOAK_NAMESPACE -f -
 
 ### ca cert (trust)
-kubectl -n $CNPG_NAMESPACE get secrets cnpg-keycloak-ca -ojson | jq -r '.data."ca.crt"' | base64 -d > cnpg-keycloak-ca-cert.pem
 if kubectl -n $KEYCLOAK_NAMESPACE get configmap cnpg-keycloak-ca; then
   kubectl -n $KEYCLOAK_NAMESPACE delete configmap cnpg-keycloak-ca
 fi
-kubectl -n $KEYCLOAK_NAMESPACE create configmap cnpg-keycloak-ca --from-file cert.pem=./cnpg-keycloak-ca-cert.pem
-rm cnpg-keycloak-ca-cert.pem
+kubectl --namespace $KEYCLOAK_NAMESPACE create configmap cnpg-keycloak-ca \
+  --from-literal=cert.pem="$(kubectl --namespace $CNPG_NAMESPACE get secrets cnpg-keycloak-ca -o jsonpath='{.data.ca\.crt}' | base64 -d)"
